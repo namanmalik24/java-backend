e2b84305b5e85cfd68092121dc889a4bfcf1d38f
namanmalik24 <naman.m888@gmail.com>
2026-02-11 15:53:20 +0000

    added a class computer

--- course/Demo.java
+++ course/Demo.java
@@ -4,12 +4,28 @@ class Calculator {
     }
 }
 
+class Computer{
+    void display(){
+        System.out.println("This is a computer.");
+    }
+
+    String getBrand(){
+        return "Generic Computer";
+    }
+}
+
 
 public class Demo {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         int result = calculator.add(5, 3);
         System.out.println("The sum is: " + result);
+
+        Computer computer = new Computer();
+        computer.display();
+        System.out.println("Brand: " + computer.getBrand());
+
+
        
     }
 }
