b2060add2c624ef1d533382ffa4f3434f1459ea4
namanmalik24 <naman.m888@gmail.com>
2026-02-11 16:21:52 +0000

    dynamic and static array

---
+++ course/arrayss.java
@@ -0,0 +1,14 @@
+class arrayss {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        int arr2[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = arr[i] * 2;
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);}
+        
+            //dynamic and static arrays
+    }
+}
+
