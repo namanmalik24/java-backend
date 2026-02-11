18a7ba67944a835d1f6933e47510ad26548e6281
namanmalik24 <naman.m888@gmail.com>
2026-02-11 14:24:34 +0000

    type conversion practice

---
+++ course/type_conversion.java
@@ -0,0 +1,10 @@
+class type_conversion{
+        public static void main(String args[]){
+            byte b=127;
+            int a =12;
+            b= (byte) a; //explicit type conversion
+
+            System.out.println(a);
+            System.out.println(b);
+        }
+}
