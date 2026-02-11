5e324f939e7ca047e6d50f015fd4a3bb69f125b8
namanmalik24 <naman.m888@gmail.com>
2026-02-11 14:24:55 +0000

    nothing special

--- course/hello.java
+++ course/hello.java
@@ -1,6 +1,16 @@
 class hello{
     public static void main(String args[]){
-        System.out.print("YES");
+        
+        int num =3;
+        //float
+        //int
+        //bool
+        //char
+
+        //double num2 = 3.14;
+        //float num3 = 3.14f;
+
+        System.out.print(num);
         System.out.println("NO");
     }
 }
